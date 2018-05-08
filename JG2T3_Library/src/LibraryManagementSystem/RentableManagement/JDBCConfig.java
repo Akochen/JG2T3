6e459e9699353166d52710e9d82ebcc6acf44bee
Yufan Lou <ylou@oldwestbury.edu>
2018-05-08 14:43:26 -0400

    Update JDBCConfig

--- JG2T3_Library/src/LibraryManagementSystem/RentableManagement/JDBCConfig.java
+++ JG2T3_Library/src/LibraryManagementSystem/RentableManagement/JDBCConfig.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 public class JDBCConfig {
 	
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/db_library?useSSL=false&autoReconnect=true";
-	private static final String USERNAME = "username";
-	private static final String PASSWORD = "password";
+	private static final String USERNAME = "root";
+	// private static final String PASSWORD = "password";
 	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		return DriverManager.getConnection(URL, USERNAME, "");
 	}
 
 }
